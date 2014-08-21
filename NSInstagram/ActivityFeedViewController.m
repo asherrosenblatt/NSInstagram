a876d7aeabafe0dcceda3bf5baf69d56b1c5c90c
Meredith Packham <m3r3dithe1ise@gmail.com>
2014-08-21 13:16:16 -0500

    think activity feed is working

--- NSInstagram/ActivityFeedViewController.m
+++ NSInstagram/ActivityFeedViewController.m
@@ -12,6 +12,7 @@
 @property (strong, nonatomic) IBOutlet UITableView *activityFeedTableView;
 @property NSMutableArray *followingArray;
 @property NSArray *photosArray;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -20,35 +21,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.followingArray = [PFUser currentUser][@"following"];
-    for (PFUser *user in [PFUser currentUser][@"following"]) {
-        [self.followingArray addObject:user];
-    }
+
+    self.followingArray = [NSMutableArray arrayWithArray:[PFUser currentUser][@"following"]];
+//    for (PFUser *user in [PFUser currentUser][@"following"]) {
+//        [self.followingArray addObject:user];
+//    }
+    NSLog(@"%@",self.followingArray);
+    [self fetchTheUsersImages];
+
 }
 
 -(void)fetchTheUsersImages
 {
-   // for (PFUser *user in self.followingArray) {
-   //     PFUser *searchString = user;
-        PFQuery *query = [PFQuery queryWithClassName:@"Photo"];
-        [query whereKey:@"user" containedIn:self.followingArray];
+  // for (PFUser *user in self.followingArray) {
+        //PFUser *searchString = user;
+       PFQuery *query = [PFQuery queryWithClassName:@"Photo"];
+      [query whereKey:@"user" containedIn:self.followingArray];
         [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-            self.photosArray = [[NSArray alloc] initWithArray:objects];
+       self.photosArray = [NSArray arrayWithArray:objects];
+             [self.tableView reloadData];
         }];
-  //  }
+   // }
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@""];
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"activityFeedCell"];
     PFFile *imageFile = [[self.photosArray objectAtIndex:indexPath.row] objectForKey:@"image"];
     [imageFile getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
         UIImage *image = [[UIImage alloc]init];
         image = [UIImage imageWithData:data];
         cell.imageView.image = image;
         NSLog(@"loaded an image");
+         [self.tableView reloadData];
     }];
     return cell;
+
+
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
